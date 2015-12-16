e00863c29b483548c3fa50fda369207d3f15494f
Anja Leskovšek <anja.leskovsek@gmail.com>
2015-12-16 10:49:04 +0100

    tabela in podatki v CSV obliki

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,20 +1,6 @@
-# 2. faza: Uvoz podatkov
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
-}
-
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
-
-obcine <- uvozi.obcine()
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+require(jsonlite)
+require(httr)
+uvoz <- GET("http://www.nhl.com/stats/rest/grouped/skaters/season/skatersummary?cayenneExp=seasonId=20142015%20and%20gameTypeId=2")
+text <- content(uvoz, "text")
+data <- fromJSON(content(uvoz, "text"))
+podatki <-data$data
