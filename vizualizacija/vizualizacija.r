39a771f9a107ebcf514cddd3f943cb35c6ccf575
Anja Leskovšek <Anja Leskovek>
2016-01-10 17:01:53 +0100

    začetek

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,65 @@
 # 3. faza: Izdelava zemljevida
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
+#Naredimo tabelo za igralce, ki so rojeni v prvi polovici
+prvi <- tabela
+prvi[prvi$mesec.rojstva >= 7,] <- NA
+prvi <- prvi[!is.na(prvi$mesec.rojstva),]
 
-# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, zemljevid, "OB_UIME", c("Ankaran", "Mirna"))
+#nareimo novo tabelo z povprečji
+prvipovprecja <- matrix(data = NA, nrow=1, ncol=6, byrow = TRUE)
+prvipovprecja <- data.frame(prvipovprecja)
+names(prvipovprecja)<- imena
 
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+prvipovprecja$odigrane.tekme<- sum(prvi$odigrane.tekme)/383
+prvipovprecja$streli <- sum(prvi$streli)/383
+prvipovprecja$goli <- sum(prvi$goli)/383
+prvipovprecja$asistence <- sum(prvi$asistence)/383
+prvipovprecja$točke <- sum(prvi$točke)/383
+prvipovprecja$procent.strela <- sum(prvi$procent.strela)/383
+
+
+#Nareimo še tabelo igralcev, ki so rojeni v ostalih mesecih
+
+drugi <- tabela
+drugi[drugi$mesec.rojstva <= 6,] <- NA
+drugi <- drugi[!is.na(drugi$mesec.rojstva),]
+
+#naredimo še njihovo povprečje
+drugipovprecja <- matrix(data = NA, nrow=1, ncol=6, byrow = TRUE)
+drugipovprecja <- data.frame(drugipovprecja)
+names(drugipovprecja)<- imena
+
+drugipovprecja$odigrane.tekme<- sum(drugi$odigrane.tekme)/304
+drugipovprecja$streli <- sum(drugi$streli)/304
+drugipovprecja$goli <- sum(drugi$goli)/304
+drugipovprecja$asistence <- sum(drugi$asistence)/304
+drugipovprecja$točke <- sum(drugi$točke)/304
+drugipovprecja$procent.strela <- sum(drugi$procent.strela)/304
+
+
+
+slika11 <- ggplot(tabela, aes(x = igralci, y=točke)) + geom_point(data=prvi, color="red") +
+  geom_point(data=drugi, color="blue") +
+  geom_hline(data=prvipovprecja, aes(yintercept=točke), color="red") +
+   geom_hline(data=drugipovprecja, aes(yintercept=točke), color="blue")
+#opazimo, da imajo igralci rojeni v drugi polovici leta boljše povprečje, čeprav ni velike razlike
+
+#zdaj pa naredimo tabelo 100 igralcev, ki imajo največ točk
+točke <- tabela
+točke <- arrange(točke, desc(točke))
+točke[točke$točke < 50,] <- NA
+točke <- točke[!is.na(točke$točke),]
+
+
+
+a.skupina <- filter(točke, mesec.rojstva > 6)
+b.skupina <- filter(točke, mesec.rojstva < 7)
+
+avsota <- sum(a.skupina$točke)
+bvsota <- sum(b.skupina$točke)
+
+  
+
+#vidimo, da je več igralcev iz druge polovice, ki imajo več točk
+#pri profesionalni ligi mesec rojstva ne vpliva toliko, kot pri mlajših fantih
+
