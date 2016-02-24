4c1c3ecf77d05186f711f288e5f4bc5b4fca7274
Anja Leskovšek <Anja Leskovek>
2016-02-24 15:33:38 +0100

    shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,2 +1,13 @@
 library(shiny)
 
+fluidPage(headerPanel('Skupine'),
+          sidebarPanel(
+            selectInput('xcol', 'X ', names(tabela4)),
+            selectInput('ycol', 'Y ', names(tabela4),
+                        selected = names(tabela4)[[2]]),
+            numericInput('clusters', 'skupine', 3,
+                         min = 1, max = 5)
+          ),
+          mainPanel(
+            plotOutput('kmeans')
+          ))
