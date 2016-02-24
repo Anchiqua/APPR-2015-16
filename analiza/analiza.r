3a9b2b4027f7310959f94a6b607ad5c24457159f
Anchiqua <anja.leskovsek@gmail.com>
2016-02-24 15:49:44 +0100

    Update analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,37 +1,37 @@
 # 4. faza: Analiza podatkov
 
 #naredimo skupine za države glede na število igralcev in točk
-tabela4 <- inner_join(tabela3, tabela2)
-rownames(tabela4) <- tabela4$drzava
-tabela4.norm <- tabela4 %>% select(-drzava) %>% scale()
+tabela5 <- inner_join(tabela3, tabela2)
+rownames(tabela5) <- tabela5$drzava
+tabela5.norm <- tabela5 %>% select(-drzava) %>% scale()
 
 
 
 
-k1 <- kmeans(tabela4.norm, 5)
+k1 <- kmeans(tabela5.norm, 3)
 #head(k$cluster, n = 15, nstart=1000)
 table(k$cluster)
-k1 <- kmeans(tabela4.norm, 5, nstart = 10000)
+k1 <- kmeans(tabela5.norm, 3, nstart = 10000)
 
-tabela4.skupine <- data.frame(Drzava = factor(names(k1$cluster),
+tabela5.skupine <- data.frame(Drzava = factor(names(k1$cluster),
                                               levels = svet$adm0_a3),
                               skupina = factor(k1$cluster))
 
-zem3 <- ggplot(tabela4.skupine %>% right_join(sv, by = c("Drzava" = "adm0_a3")),
+zem3 <- ggplot(tabela5.skupine %>% right_join(sv, by = c("Drzava" = "adm0_a3")),
        aes(x=long, y=lat, group=group, fill=skupina)) +
   geom_polygon(color="grey")
 
 
 
 #naredimo skupine igralcev glede na število točk in procent strela
-tabela5 <- tabela[c("igralci", "tocke", "procent.strela")]
-rownames(tabela5) <- tabela5$igralci
-tabela5.norm <- tabela5 %>% select(-igralci) %>% scale()
-k2 <- kmeans(tabela5.norm, 5, nstart=10000) 
-tabela5.skupine <- data.frame(tabela5, skupina = factor(k2$cluster))
-lin <- lm(data = tabela5, tocke ~ procent.strela)
+tabela6 <- tabela[c("igralci", "tocke", "procent.strela")]
+rownames(tabela6) <- tabela6$igralci
+tabela6.norm <- tabela6 %>% select(-igralci) %>% scale()
+k2 <- kmeans(tabela6.norm, 5, nstart=10000) 
+tabela6.skupine <- data.frame(tabela6, skupina = factor(k2$cluster))
+lin <- lm(data = tabela6, tocke ~ procent.strela)
 
-slika13 <- ggplot(tabela5.skupine,
+slika13 <- ggplot(tabela6.skupine,
                              aes(x=tocke, y=procent.strela, color=skupina)) + 
                               geom_point() + geom_text(aes(label=ifelse(procent.strela>20 & tocke>50,as.character(igralci),'')))
 
@@ -45,5 +45,6 @@ b.skupina <- b.skupina[!is.na(b.skupina$tocke),]
 slika14 <- ggplot(a.skupina, aes(x = tocke, y=procent.strela)) + geom_point(colour="red") + geom_smooth(method="lm")
   
 slika15 <- ggplot(b.skupina, aes(x = tocke, y=procent.strela)) + geom_point(colour="blue") + geom_smooth(method="lm")
+
 
 
