e367bfed1c45cb920bd76b4dd56ed301abcd930e
Anja Leskovšek <Anja Leskovek>
2016-02-29 08:58:08 +0100

    shiny

--- shiny/server.R
+++ shiny/server.R
@@ -2,17 +2,17 @@ library(shiny)
 
 function(input, output) {
   selectedData <- reactive({
-  tabela7.skupine[, c(input$xcol, input$ycol)]
+    tabela7.norm[, c(input$xcol, input$ycol)]
   })
-
+  
   clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
+    kmeans(tabela7.norm, input$clusters)
   })
-
+  
   output$kmeans <- renderPlot({
     par(mar = c(5.1, 4.1, 0, 1))
     plot(selectedData(),
-        col = clusters()$cluster,
-        pch = 20, cex = 3)
-      })
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+  })
 }
